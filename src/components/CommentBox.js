756b2982d4a7f175949ad545f71446c2c7749741
mythLabs <amitbisht744@gmail.com>
2018-11-26 21:08:12 +0530

    Added comment box component and started its test

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const CommentBox = () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends React.Component {
+  state = { comment: ''};
+
+  onTextChange = (e) => {
+     this.setState({comment: e.target.value});
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    //TODO- call action creator
+
+    this.setState({comment: ''})
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.onFormSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea value = {this.state.comment} onChange={this.onTextChange}/>
+        <div>
+          <button>submit a comment</button>
+        </div>
+      </form>
+    )
+  }
+}
 
 export default CommentBox;
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import App from 'components/App';
-import { shallow } from 'enzyme';
-import CommentBox from 'components/CommentBox';
-import CommentList from 'components/CommentList';
+import React from "react";
+import App from "components/App";
+import { shallow } from "enzyme";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
-it('Shows a comment box',() => {
-  const wrapped = shallow(<App />)
+let wrapped = {};
 
-  expect(wrapped.find(CommentBox).length).toEqual(1);
-  
-
+beforeEach(() => {
+  wrapped = shallow(<App />);
 });
 
-it('Shows a comment List',() => {
-  const wrapped = shallow(<App />)
+it("Shows a comment box", () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
+it("Shows a comment List", () => {
   expect(wrapped.find(CommentList).length).toEqual(1);
-  
-
 });
