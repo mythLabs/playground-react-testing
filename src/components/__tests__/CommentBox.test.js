b055f6cc6c507bfce7aecdd9d43c844fe36b841f
mythLabs <amitbisht744@gmail.com>
2018-11-27 09:54:04 +0530

    Added describe to commentbox test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,18 +18,29 @@ it("has a text area and b button", () => {
   expect(wrapped.find("button").length).toEqual(1);
 });
 
-it("it has a text are that users can type it", () => {
+describe("the text area", () => {
 
+  beforeEach(() => {
     //simulate change event ,provide fake event object
-  wrapped.find("textarea").simulate("change", {
-    target: {
-      value: 'new comment'
-    }
+    wrapped.find("textarea").simulate("change", {
+      target: {
+        value: "new comment"
+      }
+    });
+
+    // Re-render
+    wrapped.update();
   });
 
-  // Re-render
-  wrapped.update();
+  it("it has a text are that users can type it", () => {
+    //assertion
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
 
-  //assertion
-  expect(wrapped.find("textarea").prop('value')).toEqual('new comment');
+  it("comment input text area is empty after comment submittion", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
