4b5cb3a755e9fb35e02b22a7a4196f82b665f6d1
mythLabs <amitbisht744@gmail.com>
2018-11-27 09:43:00 +0530

    Added test for controlled input change

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,22 +1,35 @@
 import React from "react";
 import CommentBox from "components/CommentBox";
 import { mount } from "enzyme";
+import { wrap } from "module";
 
 let wrapped;
 
 beforeEach(() => {
-    wrapped = mount(<CommentBox />);
-})
+  wrapped = mount(<CommentBox />);
+});
 
 afterEach(() => {
-    wrapped.unmount();
-})
+  wrapped.unmount();
+});
 
+it("has a text area and b button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
 
+it("it has a text are that users can type it", () => {
 
-it('has a text area and b button',() => {
+    //simulate change event ,provide fake event object
+  wrapped.find("textarea").simulate("change", {
+    target: {
+      value: 'new comment'
+    }
+  });
 
-    expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1);
+  // Re-render
+  wrapped.update();
 
+  //assertion
+  expect(wrapped.find("textarea").prop('value')).toEqual('new comment');
 });
