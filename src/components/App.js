e22819c51f939856f434bbb8ee89b910262dd60e
mythLabs <amitbisht744@gmail.com>
2018-12-03 20:37:18 +0530

    Added routing for form and listing components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CommentBox />
-        <CommentList />
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
       </div>
     );
   }
