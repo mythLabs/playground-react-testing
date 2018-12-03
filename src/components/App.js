10bac1589038ef2b9cffe2d226356ba998835118
mythLabs <amitbisht744@gmail.com>
2018-12-03 21:14:23 +0530

    Added reducer,action for simple auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
-import { Route } from 'react-router-dom';
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class App extends Component {
+  renderHeader = () => {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a comment</Link>
+        </li>
+        <li> {this.renderButton()}</li>
+      </ul>
+    );
+  };
+
+  renderButton = () => {
+    if (this.props.auth) {
+      return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>;
+    } else {
+      return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>;
+    }
+  };
+
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div>
@@ -14,4 +39,11 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStatesToProps = state => {
+  return { auth: state.auth };
+};
+
+export default connect(
+  mapStatesToProps,
+  actions
+)(App);
