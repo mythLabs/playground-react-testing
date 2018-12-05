cc61ca4873f44bb323ca81f4ebc087502fac3a7f
mythLabs <amitbisht744@gmail.com>
2018-12-05 16:51:55 +0530

    Replaced reduxPromise with custom middleware

--- src/middleware/async.js
+++ src/middleware/async.js
@@ -1,3 +1,5 @@
+
+
 // export default function({dispatch}) {
 //   return function(next) { //<next> is reference to next middleware 
 //     return function(action) {
@@ -9,5 +11,13 @@
 // Squeezed above code
 export default ({dispatch}) => next =>  action => {
 
+  if(!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  action.payload.then((response) => {
+     const newAction = {...action, payload: response}
+     dispatch(newAction);
+  });
     
 };
--- src/root.js
+++ src/root.js
@@ -3,12 +3,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "reducers";
 import reduxPromise from 'redux-promise';
+import async from 'middleware/async';
 
 const Root = ({children,intitialState = {}}) => {
   const store = createStore(
     reducers,
     intitialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async)
   )
 
   return (
