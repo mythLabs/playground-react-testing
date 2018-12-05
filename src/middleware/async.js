47afc82f5eabdc04d849892dc1cdaa05640da5c0
mythLabs <amitbisht744@gmail.com>
2018-12-05 16:36:48 +0530

    Added middleware boilerplate

---
+++ src/middleware/async.js
@@ -0,0 +1,13 @@
+// export default function({dispatch}) {
+//   return function(next) { //<next> is reference to next middleware 
+//     return function(action) {
+
+//     };
+//   };
+// }
+
+// Squeezed above code
+export default ({dispatch}) => next =>  action => {
+
+    
+};
